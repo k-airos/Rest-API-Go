ad6e9293c20164fb667ebee7a4bd4153214b3c0d
k_airos <dr.medvedev2002@gmail.com>
2021-12-05 11:47:33 +0300

    upgrade path to config file usability

--- 5.StandartWebServer/cmd/api/main.go
+++ 5.StandartWebServer/cmd/api/main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"flag"
 	"github.com/BurntSushi/toml"
+	"github.com/joho/godotenv"
 	api "github.com/k-airos/5.StandartWebServer/internal/app"
 	"log"
+	"os"
 )
 
 var (
+	format     string
 	configPath string
 )
 
 func init() {
+	flag.StringVar(&format, "format", ".toml", "format of config file")
 	//Скажем, что наше приложение на этапе запуска будет получать путь до конфиг файла из внешнего мира
 	flag.StringVar(&configPath, "path", "configs/api.toml", "path to config file in .toml format")
 }
@@ -23,9 +27,19 @@ func main() {
 	//server instance initialization
 	config := api.NewConfig()
 	// Теперь надо попробовать из .toml/.env, т.к там может быть новая информация
-	_, err := toml.DecodeFile(configPath, config) //Десериализируете содержимое .toml файла
-	if err != nil {
-		log.Println("Can not find config file. Using default values", err)
+	if format == ".toml" {
+		_, err := toml.DecodeFile(configPath, config) //Десериализируете содержимое .toml файла
+		if err != nil {
+			log.Println("Can not find config file. Using default values", err)
+		}
+	}
+	if format == ".env" {
+		err := godotenv.Load(configPath)
+		if err != nil {
+			log.Println("Can not find config file. Using default values", err)
+		}
+		config.BindAddr = os.Getenv("bind_addr")
+		config.LoggerLevel = os.Getenv("logger_level")
 	}
 	server := api.New(config)
 
